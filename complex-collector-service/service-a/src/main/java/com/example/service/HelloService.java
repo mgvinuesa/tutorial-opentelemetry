2cdbb4914869c6d99c30af872e00c652d72bad70
ATSISTEMAS\mgarcia.devinuesa <mgarcia.devinuesa@atsistemas.com>
2022-10-10 11:00:46 +0200

    add logs to show in loki

--- complex-collector-service/service-a/src/main/java/com/example/service/HelloService.java
+++ complex-collector-service/service-a/src/main/java/com/example/service/HelloService.java
@@ -1,5 +1,7 @@
 package com.example.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,10 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class HelloService {
 
+	private static final Logger log = LoggerFactory.getLogger(HelloService.class);
+
 	@Value("#{environment.HELLO_URL}")
 	private String url;
 
 	public String sayHello() {
+		log.info("Invoking service-b from service-a to say hello");
 		return WebClient.create(url).get().uri("/hello").retrieve().bodyToMono(String.class).block();
 	}
 
--- complex-collector-service/service-b/src/main/java/com/example/controller/HelloController.java
+++ complex-collector-service/service-b/src/main/java/com/example/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
 	@GetMapping(value = "/hello")
 	public String sayHello() {
+		log.info("Saying hello from service-b");
 		return "Hello";
 	}
 }
